448de26f524ccf2693b5a4d39b6d0121321e8d3a
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2023-01-02 15:58:23 +0900

    フロント作成

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Index(TemplateView):
+    template_name = 'myapp/index.html'
