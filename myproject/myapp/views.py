df57606404ce62bb3eb19a21bd0f4a6a4236fe21
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2023-01-05 22:26:18 +0900

    データベース作成

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from .models import Post
 
 
 class Index(TemplateView):
     template_name = 'myapp/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_list = Post.objects.all().order_by('-created_at')
+        context = {
+            'post_list': post_list,
+        }
+        return context
